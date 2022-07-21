ebb70d50d04885281c6ebce076398c86537aa1f6
Younghun Kwon <kyonggi@kakao.com>
2022-07-22 04:23:49 +0900

    클래스 생성 및 필요한 함수들 가이드라인 생성 (#4)

---
+++ lolchat/substitute/substitute.py
@@ -0,0 +1,11 @@
+class Substitute:
+
+    # .txt 파일에서 문자 가져와 리스트로 반환하기
+    # def get_list(file_list):
+
+    
+    # 띄어쓰기가 제거된 리스트 생성하기
+    # def none_space_list(space_list):
+    
+    # 문장에서 리스트에 일치하는 단어가 있는지 확인하고, 인자의 단어로 치환하기
+    # def check_sub(sentense, checklist, subword):
